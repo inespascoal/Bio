025a6c256ff44a1390f0fc1c3de68372f1db9777
inespascoal <130778859+inespascoal@users.noreply.github.com>
2023-05-08 16:26:37 +0100

    Update robot2.py

--- robot2.py
+++ robot2.py
@@ -27,19 +27,25 @@ def forward():
             servo.setServoAngle(12,120-i/3)
             servo.setServoAngle(6,60+i/3) # para de trás direita faz o mesmo
             
-            #servo.setServoAngle(2,90 - i/3.6)
-            #servo.setServoAngle(10, 90 + i/3.6)
+            servo.setServoAngle(13,90 - i/3.6)
+            servo.setServoAngle(5, 90 + i/3.6)
            
         if i <= 90:
             servo.setServoAngle(3, 120 + i/9 ) # final 130
             servo.setServoAngle(9, 60 - i/9 ) # final 50
             
+            servo.setServoAngle(2,90 - i/9) # final 70
+            servo.setServoAngle(10, 90 + i/9) #final 110           
             
         # a meio do movimento das outras patas, ou seja, apos andarem 30º, as contrárias vao começar a mover-se de frente para trás
         # como andamos (i-90)/3 faz com que vá de (91-90)/3 = 1/3 até (270-90)/3 = 60
         if i > 90:
             servo.setServoAngle(3, 130 - (i-90)/2.6) # final a 60
             servo.setServoAngle(9, 50 + (i-90)/2.6 ) # final a 120
+            
+        if i > 90 and i < 100:
+            servo.setServoAngle(2, 70 + (i-90))
+            servo.setServoAngle(10, 110 - (i-90)) 
         
         time.sleep(0.01)
      
@@ -55,20 +61,27 @@ def forward():
             servo.setServoAngle(3, 60+i/3)
             servo.setServoAngle(9,120-i/3) # para de trás direita faz o mesmo
             
-            #servo.setServoAngle(2,90 - i/3.6)
-            #servo.setServoAngle(10, 90 + i/3.6)
+            servo.setServoAngle(2,90 + i/3.6)
+            servo.setServoAngle(10, 90 - i/3.6)
            
         if i <= 90:
-            servo.setServoAngle(12, 60 + i/9 ) # final 130
-            servo.setServoAngle(6, 120 - i/9 ) # final 50
+            servo.setServoAngle(12, 60 - i/9 ) # final 50
+            servo.setServoAngle(6, 120 + i/9 ) # final 130
             
+            servo.setServoAngle(13,90 + i/3.6) # final 110
+            servo.setServoAngle(5, 90 - i/3.6) # final 70    
             
         # a meio do movimento das outras patas, ou seja, apos andarem 30º, as contrárias vao começar a mover-se de frente para trás
         # como andamos (i-90)/3 faz com que vá de (91-90)/3 = 1/3 até (270-90)/3 = 60
         if i > 90:
             servo.setServoAngle(12, 130 - (i-90)/2.6) # final a 60
             servo.setServoAngle(6, 50 + (i-90)/2.6 ) # final a 120
-    
+            
+        if i > 90 and i <100:
+            servo.setServoAngle(13, 110 - (i-90)) # final a 60
+            servo.setServoAngle(5, 70 + (i-90) ) # final a 120
+        
+        time.sleep(0.01)
     
 # Main program logic follows:
 if __name__ == '__main__':
