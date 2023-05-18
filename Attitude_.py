800f94a5fcb0a9efdc413d8cb7ba9a458c5dbcbe
sofiawlss <131546955+sofiawlss@users.noreply.github.com>
2023-05-18 19:10:16 +0100

    Update Attitude_.py

--- Attitude_.py
+++ Attitude_.py
@@ -7,8 +7,8 @@
 from Control import *
 control=Control()
 
-for i in range(15):
-    control.attitude(0,0,i)
-    time.sleep(0.1)
+for i in range(10,-10,-1):
+	control.attitude(0,0,i)
+	time.sleep(0.1)
 control.stop()
 
