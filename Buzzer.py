ba45cbede07473444c3adfab511dd767047023ec
Bárbara Teixeira <130779326+babateixeira@users.noreply.github.com>
2023-05-23 18:45:32 +0100

    Update Buzzer.py

--- Buzzer.py
+++ Buzzer.py
@@ -9,16 +9,19 @@ class Buzzer:
     def run(self, d):
         buzzer_pwm = GPIO.PWM(self.Buzzer_Pin, 100)
         if (d<=20 and d>15):
-            buzzer_pwm.start(50)
+            GPIO.output(self.Buzzer_Pin,True)
+            #buzzer_pwm.start(50)
+            GPIO.output(self.Buzzer_Pin,True)
         elif (d<=15 and d>10):
-            buzzer_pwm.start(70)
+            #buzzer_pwm.start(70)
         elif (d<=10):
-            buzzer_pwm.start(100)
+            GPIO.output(self.Buzzer_Pin,True)
+            #buzzer_pwm.start(100)
             #GPIO.output(self.Buzzer_Pin,True)
     def stop(self):
-        buzzer_pwm = GPIO.PWM(self.Buzzer_Pin, 100)
-        buzzer_pwm.stop()
-        #GPIO.output(self.Buzzer_Pin,False)
+        #buzzer_pwm = GPIO.PWM(self.Buzzer_Pin, 100)
+        #buzzer_pwm.stop()
+        GPIO.output(self.Buzzer_Pin,False)
 if __name__=='__main__':
     pass
 
