57b66b5dc481a7dab376b7429c8163f7d607bf07
Bárbara Teixeira <130779326+babateixeira@users.noreply.github.com>
2023-05-25 19:53:07 +0100

    Update Buzzer.py

--- Buzzer.py
+++ Buzzer.py
@@ -6,15 +6,11 @@ class Buzzer:
         self.Buzzer_Pin = 17
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.Buzzer_Pin,GPIO.OUT)
-
     def run(self, d):
-        if d < 15:
-            GPIO.output(self.Buzzer_Pin,True)
-        
+        if (d<15):
+            GPIO.output(self.Buzzer_Pin,True) 
     def stop(self):
         GPIO.output(self.Buzzer_Pin,False)
-
-           
 
 if __name__=='__main__':
     pass
