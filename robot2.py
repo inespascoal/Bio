2f00db3382981cad5d9348e1ceb0d33251b3aba1
inespascoal <130778859+inespascoal@users.noreply.github.com>
2023-06-06 09:26:13 +0100

    Update robot2.py

--- robot2.py
+++ robot2.py
@@ -42,18 +42,22 @@ def forward():
             servo.setServoAngle(10, 111 + i/4.74) #final 130        
         
         
-
-        # a meio do movimento das outras patas, ou seja, apos andarem 30º, as contrárias vao começar a mover-se de frente para trás
+        # a meio do movimento das outras patas, ou seja, apos andarem 30º, as contrárias vão começar a mover-se de frente para trás
         
         if i > 90:
             servo.setServoAngle(3, 130 - (i-90)/3) # final a 60
             servo.setServoAngle(9, 50 + (i-90)/3 ) # final a 120
-            
+        
+        
+        
+        # mexer os servos mais pequenos -> têm de acompanhar e permitir que a pata passe de frente para trás
         if i > 90 and i < 110:
             servo.setServoAngle(2, 60 + (i-90)/4) # final 65
             servo.setServoAngle(10, 120 - (i-90)/4) # final 115
             
         if i > 110 and i < 130:
+            
+            # 70 - (110-90)/4 = 65 posição inicial do servo 2
             servo.setServoAngle(2, 70 - (i-90)/4) # final 60
             servo.setServoAngle(10, 115 + (i-90)/4) # final 120
         
@@ -69,10 +73,7 @@ def forward():
         
         time.sleep(0.03)
 
-    
-        # estará ao contrário da posição inicial
-    ################################################################
-'''
+    '''
     for i in range(270):
         
         # quando chegar a 180, ou seja, quando andar 60º as patas que estão a mover-se de trás para a frente param
@@ -92,8 +93,7 @@ def forward():
             servo.setServoAngle(13, 111 + i/9) #final 120           
         
         
-        # até aqui está a funcionar como quero!!! omddddd
-        
+
  
         # a meio do movimento das outras patas, ou seja, apos andarem 30º, as contrárias vao começar a mover-se de frente para trás
         # como andamos (i-90)/3 faz com que vá de (91-90)/3 = 1/3 até (270-90)/3 = 60
@@ -113,104 +113,6 @@ def forward():
         
         time.sleep(0.03)
 '''
-def backwards():
-    servo.setServoAngle(12,120) 
-    servo.setServoAngle(6,60) 
-    servo.setServoAngle(9,60) 
-    servo.setServoAngle(3,120)
-    
-    servo.setServoAngle(2,70) 
-    servo.setServoAngle(5,20)
-    servo.setServoAngle(10,110)
-    servo.setServoAngle(13,165) 
-    
-    servo.setServoAngle(7,75)
-    servo.setServoAngle(8,100)
-    
-    time.sleep(5)
-
-    for i in range(270):
-        
-        # quando chegar a 180, ou seja, quando andar 60º as patas que estão a mover-se de trás para a frente param
-        # como andamos (i)/3 faz com que vá de (0)/3 = 0 até (180)/3 = 60
-        if i <= 180:
-            servo.setServoAngle(9,120-i/3) # final 60
-            servo.setServoAngle(3,60+i/3) # final 120
-            
-            servo.setServoAngle(10,165 - i/3.27) #final 110
-            servo.setServoAngle(2, 20 + i/3.6) # final 70
-           
-        if i <= 90:
-            servo.setServoAngle(6, 120 + i/9 ) # final 130
-            servo.setServoAngle(12, 60 - i/9 ) # final 50
-            
-            servo.setServoAngle(5, 69 - i/9) # final 60
-            servo.setServoAngle(13, 111 + i/9) #final 120           
-        
-        
-        # até aqui está a funcionar como quero!!! omddddd
-        
- 
-        # a meio do movimento das outras patas, ou seja, apos andarem 30º, as contrárias vao começar a mover-se de frente para trás
-        # como andamos (i-90)/3 faz com que vá de (91-90)/3 = 1/3 até (270-90)/3 = 60
-        
-        if i > 90:
-            servo.setServoAngle(6, 130 - (i-90)/2.6) # final a 60
-            servo.setServoAngle(12, 50 + (i-90)/2.6 ) # final a 120
-            
-        if i > 90 and i < 110:
-            servo.setServoAngle(5, 60 - (i-90)/2) # final 50
-            servo.setServoAngle(13, 120 + (i-90)/2) # final 130
-        
-        if i > 120:
-            servo.setServoAngle(5, 50 - (i-90)/9)    # final 20
-            servo.setServoAngle(13, 130 + (i-90)/5.14) # final 165
-        
-        
-        time.sleep(0.03)
-        
-
-        ############################ estará ao contrário da posição inicial ####################################
-        
-        for i in range(270):
-        
-        # quando chegar a 180, ou seja, quando andar 60º as patas que estão a mover-se de trás para a frente param
-        # como andamos (i)/3 faz com que vá de (0)/3 = 0 até (180)/3 = 60
-            if i <= 180:
-                servo.setServoAngle(12,120-i/3) # final 60
-                servo.setServoAngle(6,60+i/3) # final 120
-
-                servo.setServoAngle(13,165 - i/3.27) #final 110
-                servo.setServoAngle(5, 20 + i/3.6) # final 70
-
-            if i <= 90:
-                servo.setServoAngle(3, 120 + i/9 ) # final 130
-                servo.setServoAngle(9, 60 - i/9 ) # final 50
-
-                servo.setServoAngle(2, 69 - i/9) # final 60
-                servo.setServoAngle(10, 111 + i/9) #final 120           
-
-
-            # até aqui está a funcionar como quero!!! omddddd
-
-
-            # a meio do movimento das outras patas, ou seja, apos andarem 30º, as contrárias vao começar a mover-se de frente para trás
-            # como andamos (i-90)/3 faz com que vá de (91-90)/3 = 1/3 até (270-90)/3 = 60
-
-            if i > 90:
-                servo.setServoAngle(3, 130 - (i-90)/2.6) # final a 60
-                servo.setServoAngle(9, 50 + (i-90)/2.6 ) # final a 120
-
-            if i > 90 and i < 110:
-                servo.setServoAngle(2, 60 - (i-90)/2) # final 50
-                servo.setServoAngle(10, 120 + (i-90)/2) # final 130
-
-            if i > 180:
-                servo.setServoAngle(2, 50 - (i-90)/6)    # final 20
-                servo.setServoAngle(10, 130 + (i-90)/5.14) # final 165
-
-
-            time.sleep(0.03)
 
 
 
@@ -226,10 +128,4 @@ if __name__ == '__main__':
         exit() 
     if sys.argv[1] == 'Forward':
         forward()
-    elif sys.argv[1] == 'Forward1':
-        forward1()
-    elif sys.argv[1] == 'Right': 
-        right()              
-    elif sys.argv[1] == 'Left':   
-        left() 
-         
+
